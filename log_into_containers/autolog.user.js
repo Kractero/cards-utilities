1a3ee3902e75b4745efc41fd2c1404e31da271ec
Kractero <115837402+Kractero@users.noreply.github.com>
2024-01-26 07:40:43 -0700

    Work for restorations

--- log_into_containers/autolog.user.js
+++ log_into_containers/autolog.user.js
@@ -2,22 +2,24 @@
 // @name        Auto Login Upload Flag
 // @namespace   Violentmonkey Scripts
 // @match       https://www.nationstates.net/*test=1*
-// @grant       none
+// @match       https://www.nationstates.net/
+// @grant       window.close
 // @version     1.0
 // @author      Kractero
 // ==/UserScript==
-(function() {
-    'use strict';
-    let enpass = document.querySelectorAll('input[type="password"]')[1];
-    if (enpass) {
-      enpass.value = "PASSWORDGLASSWORD!!!!!!!!!!!!!!!!!!!";
-      let check = document.querySelectorAll('input[type="checkbox"]')[1];
-      if (check) {
-        check.click();
-      }
-      let button = document.querySelectorAll("button")[1];
-      if (button) {
-        button.focus();
-      }
+(function () {
+  'use strict';
+  if (!document.getElementById('page_login')) return
+  let enpass = document.querySelectorAll('input[type="password"]')[1];
+  if (enpass) {
+    enpass.value = "PASSWORDGLASSWORD!!!!!!!!!!!!!!!!!!!";
+    let check = document.querySelectorAll('input[type="checkbox"]')[1];
+    if (check) {
+      check.click();
     }
+    let button = document.querySelectorAll("button")[1];
+    if (button) {
+      button.focus();
+    }
+  }
 })();
